98ce82ed50d685cdb3f0bf691bfe74ea9e2533ee
Yesica Giraldo <yagiraldot@ieee.org>
2025-07-11 21:31:46 -0500

    Creacion de tabla con SQlit y migracion con ORM

--- my_first_app/models.py
+++ my_first_app/models.py
@@ -3,3 +3,9 @@ from django.db import models
 # Create your models here.
 class Car(models.Model):
     title =models.TextField(max_length=250)
+    year=models.TextField(max_length=4, null=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.year}"
+    
+
