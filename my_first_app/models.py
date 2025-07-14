d74c9fbbf747c4eeb6c376b635167bf4cc150b87
Yesica Giraldo <yagiraldot@ieee.org>
2025-07-14 17:37:11 -0500

    mis primeras querys con django

--- my_first_app/models.py
+++ my_first_app/models.py
@@ -9,4 +9,36 @@ class Car(models.Model):
     def __str__(self):
         return f"{self.title} - {self.year} - {self.color}"
     
+class Publisher(models.Model):
+    name = models.TextField(max_length=250)
+    address = models.TextField(max_length=500, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    name = models.TextField(max_length=250)
+    birth_date = models.DateField()
+    
+    def __str__(self):
+        return self.name
+
+class Profile(models.Model):
+    author = models.OneToOneField(Author, on_delete=models.CASCADE)
+    website = models.URLField()
+    biography = models.TextField(max_length=500)
+
+    
+    
+    
+class Book(models.Model):
+    title = models.TextField(max_length=250)
+    publication_date = models.DateField()
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    authors = models.ManyToManyField(Author)
+
+    def __str__(self):
+        return f"{self.title} - {self.publication_date} - {self.publisher.name}"
+
+    
 
